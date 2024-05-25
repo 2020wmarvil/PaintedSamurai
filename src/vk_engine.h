2371df592f85d21e457bbc056a69bba039352a25
Wyatt Marvil <2020wmarvil@gmail.com>
2024-05-25 15:59:46 -0400

    Render compute shader image!

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -4,6 +4,38 @@
 #pragma once
 
 #include <vk_types.h>
+#include <vk_descriptors.h>
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+
+struct DeletionQueue
+{
+	std::deque<std::function<void()>> deletors;
+
+	void push_function(std::function<void()>&& function) {
+		deletors.push_back(function);
+	}
+
+	void flush() {
+		// reverse iterate the deletion queue to execute all the functions
+		for (auto it = deletors.rbegin(); it != deletors.rend(); it++) {
+			(*it)(); //call functors
+		}
+
+		deletors.clear();
+	}
+};
+
+struct FrameData {
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+	
+	VkSemaphore _swapchainSemaphore; // Make render commands wait on the swapchain image request
+	VkSemaphore _renderSemaphore; // Control presenting the image to the OS once drawing is complete
+	VkFence _renderFence; // Waits on draw commands for the frame to finish
+
+	DeletionQueue _deletionQueue;
+};
 
 class VulkanEngine {
 public:
@@ -28,4 +60,52 @@ public:
 
 	//run main loop
 	void run();
+
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
+	FrameData _frames[FRAME_OVERLAP];
+	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
+
+    VmaAllocator _allocator;
+	DeletionQueue _mainDeletionQueue;
+
+	//draw resources
+	AllocatedImage _drawImage;
+	VkExtent2D _drawExtent;
+
+	// descriptors
+	DescriptorAllocator globalDescriptorAllocator;
+	VkDescriptorSet _drawImageDescriptors;
+	VkDescriptorSetLayout _drawImageDescriptorLayout;
+
+	VkPipeline _gradientPipeline;
+	VkPipelineLayout _gradientPipelineLayout;
+
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+	void init_descriptors();
+	void init_pipelines();
+	void init_background_pipelines();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
+
+	void draw_background(VkCommandBuffer cmd);
 };
